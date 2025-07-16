ee144d64fc7225e27c4712a75700b54295cce211
Antonia Nenisca <91566938+antoneni2000@users.noreply.github.com>
2025-07-16 18:41:24 +0300

    Add files via upload

---
+++ homePageLogedIn.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="centru de echitatie pentru iubitori de cai in cluj">
+    <meta name="keyword" content="calarit, echitatie, cai, cal, cluj">
+    <link rel="icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="css/clrimpreuna.css">
+    <script src="js/localization.js" defer></script>
+    <title>Acasa</title>
+	</head>
+	<body>
+		<header>
+        <header>
+      
+        <nav class="navbar">
+            <div class="branding">
+                <a href="HomePage.html"><img id="logo" src="images/logo2.png" alt="logo"></a>
+            </div>
+            <div class="navbar-menu">
+                <ul>
+                    <li><a href="HomePage.html" id="nav-home">Acasă</a></li>
+                    <li><a href="educational.html" id="nav-did-you-know">Știai că?</a></li>
+                    <li><a href="galerie.html" id="nav-gallery">Galerie</a></li>
+                    <li><a href="Programari.php" id="nav-schedule">Programează-te</a></li>
+                    <li><a href="contact.html" id="nav-contact">Contactează-ne!</a></li>
+                    <li><a href="LogIn.php" id="nav-login">Intră în cont</a></li>
+                    <li><a href="sign_up.php" id="nav-signup">Fă-ți un cont</a></li>
+                </ul>
+            </div>
+            <select id="language-selector">
+                <option value="ro">Română</option>
+                <option value="en">English</option>
+                <option value="fr">Français</option>
+            </select>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+        </nav>
+</header>
+
+	</body>
+</html>
